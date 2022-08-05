949f083b40301daaed2f3166797a66948f7df9a5
zhao-stanley <contact@stanleyweb.dev>
2022-08-05 16:36:07 -0400

    Test

--- pages/_app.js
+++ pages/_app.js
@@ -17,10 +17,10 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <title>discussmed</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/static/favicons/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="title" content="discussmed" />
         <meta name="theme-color" content="#14BA86" />
