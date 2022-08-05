09049f1d05b04daa67526102f1b05dbc4f334ab9
zhao-stanley <contact@stanleyweb.dev>
2022-08-05 14:52:10 -0400

    Reformat role tagging

--- components/TeamMembers.js
+++ components/TeamMembers.js
@@ -26,9 +26,11 @@ const TeamMember = () => {
             </h1>
             <div className="flex flex-row gap-1 whitespace-nowrap">
               <span
-                className={`w-min rounded-2xl bg-gradient-to-tl ${e.tag} px-2 py-1 text-xs font-medium tracking-tight text-white`}
+                className={`w-min rounded-2xl bg-gradient-to-tl ${getRoleColor(
+                  e.role
+                )} px-2 py-1 text-xs font-medium tracking-tight text-white`}
               >
-                {e.title}
+                {getTitle(e.role)}
               </span>
             </div>
           </div>
@@ -36,6 +38,39 @@ const TeamMember = () => {
       ))}
     </ul>
   )
+}
+
+const getTitle = (role) => {
+  if (role === 'founder') {
+    return 'Founder'
+  } else if (role === 'web') {
+    return 'Webmaster'
+  } else if (role === 'research') {
+    return 'Research Coordinator'
+  } else if (role === 'content') {
+    return 'Content Writer'
+  } else {
+    return 'Community Outreach'
+  }
+}
+
+const getRoleColor = (role) => {
+  const founder = 'from-yellow-600 to-yellow-400'
+  const webmaster = 'from-[#0F3443] to-[#34E89E]'
+  const research = 'from-blue-800 to-blue-400'
+  const content = 'from-purple-800 to-purple-400'
+  const outreach = 'from-red-800 to-red-400'
+  if (role === 'founder') {
+    return founder
+  } else if (role === 'web') {
+    return webmaster
+  } else if (role === 'research') {
+    return research
+  } else if (role === 'content') {
+    return content
+  } else {
+    return outreach
+  }
 }
 
 export default TeamMember
--- data/teamData.js
+++ data/teamData.js
@@ -6,73 +6,55 @@ import maxx from '../public/static/images/team/maxx.png'
 import kevin from '../public/static/images/team/kevin.png'
 import wayne from '../public/static/images/team/wayne.png'
 
-const roles = {
-  founder: 'from-yellow-600 to-yellow-400',
-  webmaster: 'from-[#0F3443] to-[#34E89E]',
-  research: 'from-blue-800 to-blue-400',
-  content: 'from-purple-800 to-purple-400',
-  outreach: 'from-red-800 to-red-400',
-}
-
 const teamData = [
   {
     name: 'Jake Konigsberg',
-    title: 'Founder',
-    tag: roles.founder,
+    role: 'founder',
     img: jake,
   },
   {
     name: 'Stanley Zhao',
-    title: 'Webmaster',
-    tag: roles.webmaster,
+    role: 'web',
     img: stanley,
   },
   {
     name: 'Amanda Liswood',
-    title: 'Research Coordinator',
-    tag: roles.research,
+    role: 'research',
     img: amanda,
   },
   {
     name: 'Pradnya Ns',
-    title: 'Research Coordinator',
-    tag: roles.research,
+    role: 'research',
     img: placeholder,
   },
   {
     name: 'Kevin Zhang',
-    title: 'Content Writer',
-    tag: roles.content,
+    role: 'content',
     img: kevin,
   },
   {
     name: 'Wayne Shih',
-    title: 'Content Writer',
-    tag: roles.content,
+    role: 'content',
     img: wayne,
   },
   {
     name: 'Harini Senthilkumar',
-    title: 'Content Writer',
-    tag: roles.content,
+    role: 'content',
     img: placeholder,
   },
   {
     name: 'Maxx Yung',
-    title: 'Community Outreach',
-    tag: roles.outreach,
+    role: 'outreach',
     img: maxx,
   },
   {
     name: 'Xintong Lu',
-    title: 'Community Outreach',
-    tag: roles.outreach,
+    role: 'outreach',
     img: placeholder,
   },
   {
     name: 'Awab Ahmed',
-    title: 'Community Outreach',
-    tag: roles.outreach,
+    role: 'outreach',
     img: placeholder,
   },
 ]
