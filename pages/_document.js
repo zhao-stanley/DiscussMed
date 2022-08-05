e22043250c03e588d1381a653f158505e9e4a6ba
zhao-stanley <contact@stanleyweb.dev>
2022-08-05 16:37:10 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,18 +6,8 @@ class MyDocument extends Document {
       <Html lang="en" className="scroll-smooth">
         <Head>
           <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#14BA86" />
           <meta name="theme-color" content="#14BA86" />
