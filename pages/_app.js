e2179f2cdc3385e2b535f4af54e34223119b876a
zhao-stanley <contact@stanleyweb.dev>
2022-08-06 15:31:34 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ export default function App({ Component, pageProps }) {
           property="og:description"
           content="Encouraging youth engagement and interest in medical knowledge in order to promote youth based medical-related advocacy."
         />
-        <meta property="og:image" content="https://discussmed.org/static/discussmed.png" />
+        <meta property="og:image" content="https://discussmed.org/static/images/discussmed.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="discussmed.org" />
         <meta property="twitter:title" content="DiscussMed" />
@@ -43,7 +43,7 @@ export default function App({ Component, pageProps }) {
           property="twitter:description"
           content="Encouraging youth engagement and interest in medical knowledge in order to promote youth based medical-related advocacy."
         />
-        <meta property="twitter:image" content="https://discussmed.org/static/discussmed.png" />
+        <meta property="twitter:image" content="https://discussmed.org/static/images/discussmed.png" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
