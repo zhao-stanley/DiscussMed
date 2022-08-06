62d4788eb9c1e196818563ba004cb363245e77f9
zhao-stanley <contact@stanleyweb.dev>
2022-08-06 00:11:45 -0400

    Update [...slug].js

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -48,7 +48,7 @@ export default function Blog({ post, authorDetails, prev, next }) {
           next={next}
         />
       ) : (
-        <div className="mt-24 text-center">
+        <div className="mt-24 text-center py-28">
           <PageTitle>
             Under Construction{' '}
             <span role="img" aria-label="roadwork sign">
