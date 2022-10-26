00b2da68415b57ae04cb428d30d2dd3c1d3024db
Stanley Zhao <stanleyhzhao@gmail.com>
2022-10-26 16:26:39 -0400

    Update index.js

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -4,11 +4,7 @@ import siteMetadata from '@/data/siteMetadata'
 const isProduction = process.env.NODE_ENV === 'production'
 
 const Analytics = () => {
-  return (
-    <>
-      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
-    </>
-  )
+  return <>{isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}</>
 }
 
 export default Analytics
