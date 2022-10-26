2a11626b3985a2394ea6f5bb92042c5fe16151ee
zhao-stanley <stanleyhzhao@gmail.com>
2022-10-26 16:51:02 -0400

    Fix layouts I think

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,6 +1,6 @@
 export default function SectionContainer({ cn, children }) {
   return (
-    <div className={`${cn}`}>
+    <div className={cn ? `${cn}` : null}>
       <div className="mx-auto flex max-w-3xl flex-col px-4 sm:px-6 xl:max-w-5xl xl:px-0 ">
         {children}
       </div>
